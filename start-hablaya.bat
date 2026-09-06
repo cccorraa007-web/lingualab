a205a869ecacb144ff40c0648eb30d3ecc7b1de7
cora-007-cablegrama <202416016@uibe.edu.cn>
2026-09-06 16:47:29 +0800

    Switch startup script from cpolar to natapp

--- start-hablaya.bat
+++ start-hablaya.bat
@@ -1,3 +1,3 @@
 @echo off
 start "" cmd /k "cd /d D:\personal\vibe_coding_project_1 && npm run build && npm run start"
-start "" cmd /k "cd /d D:\cpolar && cpolar.exe http 3100"
+start "" cmd /k "cd /d D:\ && natapp.exe"
